ff35b1703163e913975945d0eac5ced6f3357149
Nathan Manceaux-Panot <cykelero@gmail.com>
2017-07-07 18:44:34 +0200

    Small cleanup

--- source/index.js
+++ source/index.js
@@ -1,13 +1,13 @@
-export {proxy as config} from './Config';
+export { proxy as config } from './Config';
 
-export {default as Brick}  from './Brick';
+export { default as Brick }  from './Brick';
 
-export {default as Page}  from './Page';
-export {default as Identity}  from './Identity';
+export { default as Page }  from './Page';
+export { default as Identity }  from './Identity';
 
-export {default as Chain}  from './Chain';
-export {default as CachedChain}  from './CachedChain';
-export {default as PageChain}  from './PageChain';
+export { default as Chain }  from './Chain';
+export { default as CachedChain }  from './CachedChain';
+export { default as PageChain }  from './PageChain';
 
-import {parallelForEach, arrayFromNodeList} from './Utilities';
-export let utilities = {parallelForEach, arrayFromNodeList};
+import { parallelForEach, arrayFromNodeList } from './Utilities';
+export let utilities = { parallelForEach, arrayFromNodeList };
